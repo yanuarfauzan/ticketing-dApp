851735fe8df17d4c5e214dc3e3e88449fe778a4b
yanuarfauzan <yanuarisnain017@gmail.com>
2025-07-29 15:28:48 +0700

    add mint function

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -1,4 +1,81 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract TokenMaster {}
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract TokenMaster is ERC721 {
+    address public owner;
+    address public eventOrganizer;
+    uint256 public totalSupply;
+
+    uint256 public totalEvents;
+
+    constructor(string memory _name, string memory _symbol, address _eventOrganizer) ERC721(_name, _symbol){
+        owner = msg.sender;
+        eventOrganizer = _eventOrganizer;
+    }
+
+    struct Event{
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    event EventCreated(uint256 _id, string _name, uint256 _cost, uint256 _tickets, uint256 _maxTickets, string _date, string _time, string _location);
+
+    mapping(uint256 => Event) public events;
+
+    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
+
+    mapping(uint256 => uint256[]) seatsTaken;
+
+    mapping(uint256 => mapping(address => bool)) public hasBought;
+
+    modifier onlyEo() {
+        require(msg.sender == eventOrganizer, "Only event organizer can create events");
+        _;
+    }
+
+
+    function createEvent(string memory _name, uint256 _cost, uint256 _tickets, uint256 _maxTickets, string memory _date, string memory _time, string memory _location) public onlyEo {
+        totalEvents++;
+        Event memory ev = Event(
+            totalEvents,
+            _name,
+            _cost,
+            _tickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+
+        events[totalEvents] = ev;
+
+        emit EventCreated(totalEvents, _name, _cost, _tickets, _maxTickets, _date, _time, _location);
+    }
+
+    function getSeatTakens(uint256 _id) public view returns(uint256[] memory){
+        return seatsTaken[_id];
+    }
+
+    function getEvents(uint256 _id) public view returns(Event memory){
+        return events[_id];
+    }
+
+    function mint(uint256 _id, uint256 _seat) public payable {
+        events[_id].tickets -= 1;
+        hasBought[_id][msg.sender] = true;
+        seatTaken[_id][_seat] = msg.sender;
+        seatsTaken[_id].push(_seat);
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
+    }
+
+
+}
--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,121 @@
 const { expect } = require("chai")
 
-describe("TokenMaster", () => {
+const tokens = (n)=> {
+    return ethers.utils.parseUnits(n.toString(), 'ether');
+}
 
+describe("TokenMaster", () => {
+    let deployer, eventOrganizer, user, tokenMaster, transaction;
+    const NAME = "TokenMaster";
+    const SYMBOL = "TM";
+    const EVENT = "Taylor Swift Concert";
+    const COST = tokens(1);
+    const TICKETS = 100;
+    const MAX_TICKETS = 100;
+    const DATE = "2021-01-01";
+    const TIME = "20:00";
+    const LOCATION = "New York";
+    describe("Deployment", () => {
+        beforeEach(async () => {
+            [deployer, eventOrganizer, user] = await ethers.getSigners();
+            const TokenMaster = await ethers.getContractFactory("TokenMaster");
+            tokenMaster = await TokenMaster.connect(deployer).deploy(NAME, SYMBOL, eventOrganizer.address);
+        
+            transaction = await tokenMaster.connect(eventOrganizer).createEvent(EVENT, COST, TICKETS, MAX_TICKETS, DATE, TIME, LOCATION
+            )
+            await transaction.wait();
+        })
+
+        it('sets the name', async () => {
+            const name = await tokenMaster.name();
+            expect(name).to.be.equal(NAME);
+        })
+
+        it('sets the symbol', async () => {
+            const symbol = await tokenMaster.symbol();
+            expect(symbol).to.be.equal(SYMBOL);
+        })
+
+        it('sets the owner', async () => {
+            const owner = await tokenMaster.owner();
+            expect(owner).to.be.equal(deployer.address);
+        })
+    })
+
+    describe('Create Events', async () => {
+        beforeEach(async () => {
+            [deployer, eventOrganizer, user] = await ethers.getSigners();
+            const TokenMaster = await ethers.getContractFactory("TokenMaster");
+            tokenMaster = await TokenMaster.connect(deployer).deploy(NAME, SYMBOL, eventOrganizer.address);
+        
+            transaction = await tokenMaster.connect(eventOrganizer).createEvent(EVENT, COST, TICKETS, MAX_TICKETS, DATE, TIME, LOCATION
+            )
+            await transaction.wait();
+        })
+
+        it('Update total events', async () => {
+            const totalEvents = await tokenMaster.totalEvents();
+            expect(totalEvents).to.be.equal(1);
+        })
+
+        it('Sets the attributes', async () => {
+            const event = await tokenMaster.getEvents(1);
+            expect(event.id).to.be.equal(1);
+            expect(event.name).to.be.equal(EVENT);
+            expect(event.cost).to.be.equal(COST);
+            expect(event.tickets).to.be.equal(TICKETS);
+            expect(event.maxTickets).to.be.equal(MAX_TICKETS);
+            expect(event.date).to.be.equal(DATE);
+            expect(event.time).to.be.equal(TIME);
+            expect(event.location).to.be.equal(LOCATION);
+        })
+    })
+
+    describe('Minting', async () => {
+        const ID = 1;
+        const SEAT = 50;
+        const AMOUNT = tokens(1);
+
+        beforeEach(async () => {
+            [deployer, eventOrganizer, user] = await ethers.getSigners();
+            const TokenMaster = await ethers.getContractFactory("TokenMaster");
+            tokenMaster = await TokenMaster.connect(deployer).deploy(NAME, SYMBOL, eventOrganizer.address);
+        
+            transaction = await tokenMaster.connect(eventOrganizer).createEvent(EVENT, COST, TICKETS, MAX_TICKETS, DATE, TIME, LOCATION
+            )
+            await transaction.wait();
+
+            transaction = await tokenMaster.connect(user).mint(ID, SEAT, {value: AMOUNT});
+            await transaction.wait();
+        })
+
+        it('Updates ticket counts', async () => {
+            const totalEvents = await tokenMaster.getEvents(1);
+            expect(totalEvents.tickets).to.be.equal(TICKETS - 1);
+        })
+
+        it('Updates buying status', async () => {
+            const hasBought = await tokenMaster.hasBought(ID, user.address);
+            expect(hasBought).to.be.equal(true);
+        })
+
+        it('Updates seatTaken status', async () => {
+            const owner = await tokenMaster.seatTaken(ID, SEAT);
+            expect(owner).to.be.equal(user.address);
+        })
+
+        it('Updates overall seatTaken', async () => {
+            const seats = await tokenMaster.getSeatTakens(ID);
+            expect(seats.length).to.equal(1);
+            expect(seats[0]).to.be.equal(SEAT);
+        })
+
+        it('Updates contract balance', async () => {
+            const balance = await ethers.provider.getBalance(tokenMaster.address);
+            expect(balance).to.be.equal(AMOUNT);
+        })
+
+
+
+    })
 })
